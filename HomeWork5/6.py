77460b377b12d5bc48eb48e932db0ab662d36a5e
Vladislav <79606232106@yandex.ru>
2021-01-18 14:31:55 +0300

    Create HomeWork5_2

--- HomeWork5/6.py
+++ HomeWork5/6.py
@@ -1,8 +1,19 @@
 data = {}
 with open('file_6.txt', 'r', encoding='utf-8') as el:
     for line in el:
-       a, b, c, d = line.split()
+        content = line.split()
+        key = content[0].strip(':')
+        total = 0
+        for elem in content:
+            num = '0'
+            for i in elem:
+                if i.isdigit():
+                    num += i
+            total += int(num)
 
-       data[a] = int(b) + int(c) + int(d)
-print(f"Общее кол- во часов по предмету - ")
+        data[key] = total
+print(data)
+
+
+
 
